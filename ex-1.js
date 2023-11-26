b004d7727be667d0bc681697f10fdc614f162d34
Dennis <suwatchai.kaew@gmail.com>
2023-11-26 15:37:12 +0700

    feat: add ex1-2 v2 (dennis)

--- ex-1.js
+++ ex-1.js
@@ -1,17 +1,17 @@
 // Exercise #1: Strict and Same
 // Start commenting here
 Object.is(10, 20); // false
-Object.is(0, -0); // true
+Object.is(0, -0); // false
 Object.is(NaN, 100); // false
 Object.is("one", "two"); // false
 Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); // true
 Object.is(null, undefined); // false
 
 10 === 20; // false
-10 === "10" // false
-3.14 !== 3.14; //false
+10 === "10"; // false
+3.14 !== 3.14; // false
 "TechUp" !== "TechUp"; //false
-NaN === NaN; // true
+NaN === NaN; // false
 0 !== -0; // false
 
 let objectA = { course: "Web Developer", price: 300000 };
